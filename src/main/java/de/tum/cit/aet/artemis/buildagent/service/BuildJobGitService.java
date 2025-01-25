bd010872782bd9bcfea1d9b83a3612523b8005b9
Abuenameh <abuenameh@gmail.com>
2025-01-25 16:47:45 -0400

    Update BuildJobGitService.java

--- src/main/java/de/tum/cit/aet/artemis/buildagent/service/BuildJobGitService.java
+++ src/main/java/de/tum/cit/aet/artemis/buildagent/service/BuildJobGitService.java
@@ -158,6 +158,7 @@ public class BuildJobGitService extends AbstractGitService {
     }
 
     private CredentialsProvider getCachedCredentialsProvider() {
+        log.warn("Build agent Git credentials: {} {}", buildAgentGitUsername, buildAgentGitPassword);
         if (credentialsProvider == null) {
             credentialsProvider = new UsernamePasswordCredentialsProvider(buildAgentGitUsername, buildAgentGitPassword);
         }
